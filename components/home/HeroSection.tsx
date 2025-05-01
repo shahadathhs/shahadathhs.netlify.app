68822ef1020da1973d0a033ef58b240047e3b06e
shahadathhs <shahadathhossensajib732@gmail.com>
2025-05-01 16:55:42 +0600

    UI: hero section completed

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -3,8 +3,15 @@
 import { motion } from "motion/react";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import { Typewriter } from "react-simple-typewriter";
 
 export default function HeroSection() {
+  const firstLine = "Hey there, I'm".split(" ");
+  const secondLine = "Shahadath Hossen Sajib".split(" ");
+  const stagger = 0.2; // seconds between each word
+  const duration = 0.6; // each word’s animation duration
+  const firstTotal = firstLine.length * stagger + duration;
+
   return (
     <div className="relative mx-auto my-10 flex max-w-7xl flex-col items-center justify-center">
       <div className="absolute inset-y-0 left-0 h-full w-px bg-neutral-200/80 dark:bg-neutral-800/80">
@@ -17,15 +24,31 @@ export default function HeroSection() {
         <div className="absolute mx-auto h-px w-40 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
       </div>
       <div className="px-4 py-10 md:py-20">
-        <h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 md:text-4xl lg:text-7xl dark:text-slate-300">
-          {"Hey there, I'm Sajib".split(" ").map((word, index) => (
+        <h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 md:text-4xl lg:text-6xl dark:text-slate-300">
+          {firstLine.map((word, i) => (
             <motion.span
-              key={index}
+              key={`f-${i}`}
               initial={{ opacity: 0, filter: "blur(4px)", y: 10 }}
               animate={{ opacity: 1, filter: "blur(0px)", y: 0 }}
               transition={{
-                duration: 0.3,
-                delay: index * 0.1,
+                duration,
+                delay: i * stagger,
+                ease: "easeInOut",
+              }}
+              className="mr-2 inline-block"
+            >
+              {word}
+            </motion.span>
+          ))}
+          <br />
+          {secondLine.map((word, i) => (
+            <motion.span
+              key={`s-${i}`}
+              initial={{ opacity: 0, filter: "blur(4px)", y: 10 }}
+              animate={{ opacity: 1, filter: "blur(0px)", y: 0 }}
+              transition={{
+                duration,
+                delay: firstTotal + i * stagger,
                 ease: "easeInOut",
               }}
               className="mr-2 inline-block"
@@ -45,10 +68,16 @@ export default function HeroSection() {
             duration: 0.3,
             delay: 0.8,
           }}
-          className="relative z-10 mx-auto max-w-xl py-4 text-center text-lg font-normal text-neutral-600 dark:text-neutral-400"
+          className="relative z-10 mx-auto max-w-xl py-4 text-center text-3xl font-normal text-neutral-600 dark:text-neutral-400"
         >
-          I am a junior backend developer with a passion for creating efficient
-          and scalable web applications.
+          <Typewriter
+            words={["Junior Backend Developer", "System Design Enthusiast"]}
+            loop={Infinity}
+            cursor
+            typeSpeed={100}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
         </motion.p>
 
         {/* cta buttons */}
@@ -63,7 +92,7 @@ export default function HeroSection() {
             duration: 0.3,
             delay: 1,
           }}
-          className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
+          className="relative z-10 mt-4 flex flex-wrap items-center justify-center gap-4"
         >
           <button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
             <Link href="/projects">See My Work</Link>
@@ -85,9 +114,14 @@ export default function HeroSection() {
             duration: 0.3,
             delay: 1,
           }}
-          className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
+          className="relative z-10 mt-4 flex flex-wrap items-center justify-center gap-4"
         >
-          <button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
+          <Button
+            asChild
+            variant="outline"
+            size="lg"
+            className="hover:scale-120 transition-transform duration-500"
+          >
             <Link
               target="_blank"
               href="https://drive.google.com/file/d/1dtZCEgZyof-qrUreeVpXDlOovosegpuf/view"
@@ -95,7 +129,7 @@ export default function HeroSection() {
             >
               View My Resume
             </Link>
-          </button>
+          </Button>
         </motion.div>
       </div>
     </div>
